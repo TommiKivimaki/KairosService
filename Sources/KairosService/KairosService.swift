96f042bd3ec7364d2a16195551fd3c6180f85cca
Tommi Kivimäki <tommi@tommikivimaki.com>
2019-08-28 13:39:23 +0300

    healthCheck healthStatus

--- Sources/KairosService/KairosService.swift
+++ Sources/KairosService/KairosService.swift
@@ -1,8 +1,10 @@
 import Foundation
+import HTTP
 import Vapor
 
 public protocol KairosProvider: Service {
-  func check(on container: Container) throws -> Future<Response>
+  func healthStatus(on container: Container) throws -> Future<Response>
+  func healthCheck(on container: Container) throws -> Future<Response>
 }
 
 public struct Kairos: KairosProvider {
@@ -20,9 +22,15 @@ public struct Kairos: KairosProvider {
     return text
   }
   
-  public func check(on container: Container) throws -> Future<Response> {
+  public func healthStatus(on container: Container) throws -> Future<Response> {
     return try getRequest(endpoint: "health/status", on: container)
   }
+  
+  public func healthCheck(on container: Container) throws -> Future<Response> {
+    return try getRequest(endpoint: "health/check", on: container)
+  }
+  
+
 
 }
 
@@ -34,8 +42,16 @@ extension Kairos {
     let checkUrlPath = "\(demoBaseUrlPath)/\(endpoint)"
     print(checkUrlPath)
     
+    // Headers to send to remote API
+    var headers: HTTPHeaders = [:]
+    // Copy from client request or if missing use defaults
+    headers.add(name: .accept, value: "application/json")
+    headers.add(name: .acceptEncoding, value: "deflate")
+    
     let client = try container.make(Client.self)
     
     return client.get(checkUrlPath)
+
   }
+  
 }
