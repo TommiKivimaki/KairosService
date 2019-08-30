27dec825930ee2355a60e4f67c57a820a9fa7116
Tommi Kivimäki <tommi@tommikivimaki.com>
2019-08-30 09:12:59 +0300

    Public extension

--- Sources/KairosService/Models/Message.swift
+++ Sources/KairosService/Models/Message.swift
@@ -8,21 +8,28 @@
 import Foundation
 import Vapor
 
-extension Kairos {
-  
+public extension Kairos {
+
   public struct Message: Content {
-    
-    public let start_Absolute: Int
+    public let start_absolute: Int
     public let end_absolute: Int
     public let metrics: [Metric]
+    
+    public init(start_absolute: Int, end_absolute: Int, metrics: [Metric]) {
+      self.start_absolute = start_absolute
+      self.end_absolute = end_absolute
+      self.metrics = metrics
+    }
   }
   
   
   public struct Metric: Content {
-    
     public let name: String
     public let limit: Int
+    
+    public init(name: String, limit: Int) {
+      self.name = name
+      self.limit = limit
+    }
   }
 }
-
-
